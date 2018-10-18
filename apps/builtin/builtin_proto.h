675be66df866c66f4ed5bafe72bb2cf8d94515e0
lanpinguo <lanpinguo1985@126.com>
2018-10-18 17:07:36 +0800

    add nuttx

---
+++ apps/builtin/builtin_proto.h
@@ -0,0 +1,8 @@
+int stackmonitor_stop_main(int argc, char *argv[]);
+int ostest_main(int argc, char *argv[]);
+int alarm_main(int argc, char *argv[]);
+int stackmonitor_start_main(int argc, char *argv[]);
+int rand_main(int argc, char *argv[]);
+int adc_main(int argc, char *argv[]);
+int i2c_main(int argc, char *argv[]);
+int tee_main(int argc, char *argv[]);
